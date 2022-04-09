101419e2fab7dc8f753f5b1e8d04ba54ae738ff4
astr0n0mer <imran2000786@gmail.com>
2022-04-09 20:34:29 +0530

    Added Animation for Maze generation & solving

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -22,6 +24,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * Creates a random maze, then solves it by finding a path from the upper left
@@ -30,7 +33,7 @@ import javafx.stage.Stage;
  * visualize the process.
  */
 
-public class Main extends Application implements Runnable {
+public class Main extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
@@ -57,10 +60,8 @@ public class Main extends Application implements Runnable {
 	Canvas canvas; // the canvas where the maze is drawn and which fills the whole window
 	GraphicsContext g; // graphics context for drawing on the canvas
 
-	// h --> last color changed
 	Color color[];
 
-	// colors associated with the preceding 5 constants;
 	// int rows = 31; // number of rows of cells in maze, including a wall around
 	// edges
 	// int columns = 41; // number of columns of cells in maze, including a wall
@@ -73,10 +74,11 @@ public class Main extends Application implements Runnable {
 	Color wallColor;
 
 	// animation stuff
-	int sleepTime = 4000; // wait time after solving one maze before making another, in milliseconds
-	int speedSleep = 20; // short delay between steps in making and solving maze
+//	int sleepTime = 5; // wait time after solving one maze before making another, in milliseconds
+//	int speedSleep = 20; // short delay between steps in making and solving maze
 
 	public void start(Stage stage) {
+
 		// Made changes here
 		color = new Color[] { Color.rgb(200, 0, 0), Color.rgb(200, 0, 0), Color.rgb(128, 128, 255), Color.WHITE,
 				Color.rgb(200, 200, 200) };
@@ -86,37 +88,23 @@ public class Main extends Application implements Runnable {
 		Group group = new Group(menu);
 
 		VBox container1 = new VBox();
-//		VBox container2 = new VBox();
-//		Scene scene1 = new Scene(container1, 1550, 800);
 		Scene scene1 = new Scene(container1, 1000, 600);
 		Scene scene2 = new Scene(group, scene1.getWidth(), scene1.getHeight());
-//		Scene scene3;
 
 		// Scene 1
 		Label label1 = new Label("Maze Runner");
-		// label1.setFont(new Font("Consolas", 60));
 		label1.setTextFill(Color.CORNFLOWERBLUE);
-
-		// label1.setLayoutX(485);
-		// label1.setLayoutY(270);
 		label1.setFont(new Font("forte", 105));
 		;
 
-		// Button btn1 = new Button("Go to Scene 2");
-		// btn1.setOnAction(e -> stage.setScene(scene2));
-
 		Label label2 = new Label("Press Any Key to Continue");
-		Font font = Font.font("Agency FB", FontWeight.BOLD, 30);
-
-		label2.setFont(font);
+		Font welcomeFont = Font.font("Agency FB", FontWeight.BOLD, 30);
+		label2.setFont(welcomeFont);
+		;
 		label2.setTextFill(Color.GREY);
 
 		container1.getChildren().addAll(label1, label2);
-		// container1.setLayoutX(485);
-		// container1.setLayoutY(270);
 		container1.setAlignment(Pos.CENTER);
-		// container1.backgroundProperty();
-		// scene1.setFill(Color.DARKGREY);
 
 		scene1.setOnKeyPressed((KeyEvent ke) -> stage.setScene(scene2));
 
@@ -127,19 +115,14 @@ public class Main extends Application implements Runnable {
 		int rowCounter = 0;
 
 		Label xCellsLabel = new Label("Cells in each row:");
-
-		Font font2 = Font.font("Agency FB", FontWeight.BOLD, 35);
-		xCellsLabel.setFont(font2);
+		Font normalFont = Font.font("Agency FB", FontWeight.BOLD, 35);
+		xCellsLabel.setFont(normalFont);
 
 		Label yCellsLabel = new Label("Cells in each column:");
-
-		Font font3 = Font.font("Agency FB", FontWeight.BOLD, 35);
-		yCellsLabel.setFont(font3);
+		yCellsLabel.setFont(normalFont);
 
 		Label passageLengthLabel = new Label("Passage length (px):");
-
-		Font font4 = Font.font("Agency FB", FontWeight.BOLD, 35);
-		passageLengthLabel.setFont(font4);
+		passageLengthLabel.setFont(normalFont);
 
 		Spinner<Integer> xCellsInput = new Spinner<>(2, 100, 20);
 		xCellsInput.setEditable(true);
@@ -158,17 +141,12 @@ public class Main extends Application implements Runnable {
 		menu.addRow(rowCounter++, xCellsLabel, xCellsInput);
 		menu.addRow(rowCounter++, yCellsLabel, yCellsInput);
 		menu.addRow(rowCounter++, passageLengthLabel, passageLengthInput);
-//		menu.addRow(rowCounter++, wallLengthLabel, wallThicknessInput);
 
 		Label passageColorLabel = new Label("Passage Color:");
-
-		Font font5 = Font.font("Agency FB", FontWeight.BOLD, 35);
-		passageColorLabel.setFont(font5);
+		passageColorLabel.setFont(normalFont);
 
 		Label wallColorLabel = new Label("Wall Color:");
-
-		Font font6 = Font.font("Agency FB", FontWeight.BOLD, 35);
-		wallColorLabel.setFont(font6);
+		wallColorLabel.setFont(normalFont);
 
 		ColorPicker passageColorPicker = new ColorPicker(Color.RED);
 		passageColorPicker.setScaleX(1.15);
@@ -181,35 +159,29 @@ public class Main extends Application implements Runnable {
 		menu.addRow(rowCounter++, passageColorLabel, passageColorPicker);
 		menu.addRow(rowCounter++, wallColorLabel, wallColorPicker);
 
-		// maze = new int[xCellsInput.getValue()][yCellsInput.getValue()];
+		Button generateMazeBtn = new Button("Generate Maze");
+		generateMazeBtn.setLayoutX((scene2.getWidth() / 2) - 300);
+		generateMazeBtn.setLayoutY(scene2.getHeight() - 150);
+		generateMazeBtn.setScaleX(1.15);
+		generateMazeBtn.setScaleY(1.15);
 
-		Button btn2 = new Button("Generate Maze");
-		btn2.setLayoutX((scene2.getWidth() / 2) - 300);
-		btn2.setLayoutY(scene2.getHeight() - 150);
-		btn2.setScaleX(1.15);
-		btn2.setScaleY(1.15);
+		Font buttonTextFont = Font.font("Agency FB", FontWeight.BOLD, 28);
+		generateMazeBtn.setFont(buttonTextFont);
 
-		Font font7 = Font.font("Agency FB", FontWeight.BOLD, 28);
-		btn2.setFont(font7);
+		group.getChildren().add(generateMazeBtn);
 
-		group.getChildren().add(btn2);
-		// menu.addRow(rowCounter++, btn2);
-
-		btn2.setOnAction(new EventHandler<ActionEvent>() {
+		generateMazeBtn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-
 				if ((xCellsInput.getValue() & 1) == 1)
-					rows = xCellsInput.getValue();
-
+					columns = xCellsInput.getValue();
 				else
-					rows = xCellsInput.getValue() + 1;
+					columns = xCellsInput.getValue() + 1;
 
 				if ((yCellsInput.getValue() & 1) == 1)
-					columns = yCellsInput.getValue();
-
+					rows = yCellsInput.getValue();
 				else
-					columns = yCellsInput.getValue() + 1;
+					rows = yCellsInput.getValue() + 1;
 
 				blockSize = passageLengthInput.getValue();
 				passageColor = passageColorPicker.getValue();
@@ -222,39 +194,47 @@ public class Main extends Application implements Runnable {
 				g = canvas.getGraphicsContext2D();
 				g.setFill(wallColor);
 				g.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
 				Pane root = new Pane(canvas);
 				ScrollPane sp = new ScrollPane();
 				sp.setLayoutY(30);
 				sp.setLayoutX(20);
-//				sp.setPrefSize(scene2.getWidth(), scene2.getHeight());
+				sp.setPrefSize(scene2.getWidth() - generateMazeBtn.getWidth(), scene2.getHeight() - 50);
 
 				sp.setContent(root);
 
-				Button back = new Button("Main Menu");
-				back.setLayoutX(scene2.getWidth() - 300);
-				back.setLayoutY(30);
-				back.setScaleX(1.15);
-				back.setScaleY(1.15);
+				Button solveMazeBtn = new Button("Solve Maze");
 
-				Font font8 = Font.font("Agency FB", FontWeight.BOLD, 28);
-				back.setFont(font8);
+				solveMazeBtn.setLayoutX(sp.getLayoutX() + sp.getPrefWidth() + 10);
+				solveMazeBtn.setLayoutY(sp.getLayoutY() + 10);
+				solveMazeBtn.setScaleX(1.15);
+				solveMazeBtn.setScaleY(1.15);
 
-				back.setOnAction(ev -> stage.setScene(scene2));
+//				Font font9 = Font.font("Agency FB", FontWeight.BOLD, 28);
+				solveMazeBtn.setFont(buttonTextFont);
+
+				solveMazeBtn.setOnAction(ev -> solveMaze(1, 1, 100));
+
+				Button mainMenuBtn = new Button("Main Menu");
+				mainMenuBtn.setLayoutX(solveMazeBtn.getLayoutX());
+				mainMenuBtn.setLayoutY(sp.getLayoutY() + 100);
+				mainMenuBtn.setScaleX(1.15);
+				mainMenuBtn.setScaleY(1.15);
+
+//				Font font8 = Font.font("Agency FB", FontWeight.BOLD, 28);
+				mainMenuBtn.setFont(buttonTextFont);
+
+				mainMenuBtn.setOnAction(ev -> stage.setScene(scene2));
 
 				Group group2 = new Group(sp);
 
-				group2.getChildren().add(back);
-
-//				Thread runner = new Thread(new Main());
-//				runner.setDaemon(true); // so thread won't stop program from ending
-//				runner.start();
+				group2.getChildren().addAll(solveMazeBtn, mainMenuBtn);
 
 				Scene scene3 = new Scene(group2, scene1.getWidth(), scene1.getHeight());
-				// scene3.setFill(Color.GRAY);
 
 				stage.setScene(scene3);
 
-				run();
+				makeMaze();
 
 			}
 		});
@@ -264,55 +244,17 @@ public class Main extends Application implements Runnable {
 		stage.setTitle("Maze Runner");
 		stage.show();
 
-//		Thread runner = new Thread(this);
-//		//runner.setDaemon(true); // so thread won't stop program from ending
-//		runner.start();
 	}
 
 	void drawSquare(int row, int column, int colorCode) {
 		// Fill specified square of the grid with the
 		// color specified by colorCode, which has to be
 		// one of the constants emptyCode, wallCode, etc.
-//		
-//		Platform.runLater(() -> {
-//			g.setFill(color[colorCode]);
-//			int x = blockSize * column;
-//			int y = blockSize * row;
-//			g.fillRect(x, y, blockSize, blockSize);
-//		});
-//		
+
 		g.setFill(color[colorCode]);
 		int x = blockSize * column;
 		int y = blockSize * row;
 		g.fillRect(x, y, blockSize, blockSize);
-
-	}
-
-	public void run() {
-		// Run method for thread repeatedly makes a maze and then solves it.
-		// Note that all access to the canvas by the thread is done using
-		// Platform.runLater(), so that all drawing to the canvas actually
-		// takes place on the application thread.
-
-//        while (true) {
-//		try {
-//			Thread.sleep(1000);
-//		} // wait a bit before starting
-//		catch (InterruptedException e) {
-//		}
-
-		makeMaze();
-		solveMaze(1, 1);
-
-//            synchronized(this) {
-//                try { wait(sleepTime); }
-//                catch (InterruptedException e) { }
-//            }
-//            Platform.runLater( () -> {
-//                g.setFill(color[backgroundCode]);
-//                g.fillRect(0,0,canvas.getWidth(),canvas.getHeight());
-//            });
-//        }
 
 	}
 
@@ -348,48 +290,48 @@ public class Main extends Application implements Runnable {
 			}
 		}
 
-//		Platform.runLater(() -> {
-//			g.setFill(color[emptyCode]);
-//			for (int r = 0; r < rows; r++) {
-//				for (int c = 0; c < columns; c++) {
-//					if (maze[r][c] < 0)
-//						g.fillRect(c * blockSize, r * blockSize, blockSize, blockSize);
-//				}
-//			}
-//		});
-//		
-//        synchronized(this) {
-//            try { wait(1000); }
-//            catch (InterruptedException e) { }
-//        }
-
 		g.setFill(color[emptyCode]);
+
+		int ct = 50;
+
 		for (int r = 0; r < rows; r++) {
 			for (int c = 0; c < columns; c++) {
-				if (maze[r][c] < 0)
-					g.fillRect(c * blockSize, r * blockSize, blockSize, blockSize);
+				if (maze[r][c] < 0) {
+
+					int f = c;
+					int m = r;
+
+					Timeline t2 = new Timeline(new KeyFrame(Duration.millis(500 + ct),
+							e -> g.fillRect(f * blockSize, m * blockSize, blockSize, blockSize)));
+					t2.play();
+
+					ct += 50;
+				}
 			}
 		}
 
 		int r;
 		for (i = wallCt - 1; i > 0; i--) {
 			r = (int) (Math.random() * i); // choose a wall randomly and maybe tear it down
-			tearDown(wallrow[r], wallcol[r]);
+
+			tearDown(wallrow[r], wallcol[r], 500 + ct);
+
 			wallrow[r] = wallrow[i];
 			wallcol[r] = wallcol[i];
+
+			ct += 100;
 		}
 
-		for (i = 1; i < rows - 1; i++) // replace negative values in maze[][] with emptyCode
-			for (j = 1; j < columns - 1; j++)
+		for (i = 1; i < rows - 1; i++) { // replace negative values in maze[][] with emptyCode
+			for (j = 1; j < columns - 1; j++) {
 				if (maze[i][j] < 0)
 					maze[i][j] = emptyCode;
-//        synchronized(this) {
-//            try { wait(1000); }
-//            catch (InterruptedException e) { }
+			}
 
+		}
 	}
 
-	void tearDown(int row, int col) {
+	void tearDown(int row, int col, int ex) {
 		// Tear down a wall, unless doing so will form a loop. Tearing down a wall
 		// joins two "rooms" into one "room". (Rooms begin to look like corridors
 		// as they grow.) When a wall is torn down, the room codes on one side are
@@ -403,10 +345,6 @@ public class Main extends Application implements Runnable {
 			fill(row, col - 1, maze[row][col - 1], maze[row][col + 1]);
 			maze[row][col] = maze[row][col + 1];
 			drawSquare(row, col, emptyCode);
-//            synchronized(this) {
-//                try { wait(speedSleep); }
-//                catch (InterruptedException e) { }
-//            }
 		}
 
 		else if (row % 2 == 0 && maze[row - 1][col] != maze[row + 1][col]) {
@@ -415,10 +353,6 @@ public class Main extends Application implements Runnable {
 			fill(row - 1, col, maze[row - 1][col], maze[row + 1][col]);
 			maze[row][col] = maze[row + 1][col];
 			drawSquare(row, col, emptyCode);
-//            synchronized(this) {
-//                try { wait(speedSleep); }
-//                catch (InterruptedException e) { }
-//            }
 		}
 	}
 
@@ -436,43 +370,31 @@ public class Main extends Application implements Runnable {
 		}
 	}
 
-	boolean solveMaze(int row, int col) {
+	boolean solveMaze(int row, int col, int ct) {
 		// Try to solve the maze by continuing current path from position
 		// (row,col). Return true if a solution is found. The maze is
 		// considered to be solved if the path reaches the lower right cell.
-
 		if (maze[row][col] == emptyCode) {
 			maze[row][col] = pathCode; // add this cell to the path
-			drawSquare(row, col, pathCode);
+
+			Timeline t = new Timeline(new KeyFrame(Duration.millis(500 + ct), e -> drawSquare(row, col, pathCode)));
+			t.play();
+			ct += 100;
+
 			if (row == rows - 2 && col == columns - 2)
 				return true; // path has reached goal
-//            try { Thread.sleep(speedSleep); }
-//            catch (InterruptedException e) { }
-			if (solveMaze(row - 1, col) || // try to solve maze by extending path
-					solveMaze(row, col - 1) || // in each possible direction
-					solveMaze(row + 1, col) || solveMaze(row, col + 1))
+
+			if (solveMaze(row - 1, col, ct) || // try to solve maze by extending path
+					solveMaze(row, col - 1, ct) || // in each possible direction
+					solveMaze(row + 1, col, ct) || solveMaze(row, col + 1, ct))
 				return true;
-			// maze can't be solved from this cell, so backtrack out of the cell
 
 			maze[row][col] = visitedCode; // mark cell as having been visited
-			drawSquare(row, col, visitedCode);
-//            synchronized(this) {
-//                try { wait(speedSleep); }
-//                catch (InterruptedException e) { }
-//            }
+
+			Timeline t2 = new Timeline(new KeyFrame(Duration.millis(500 + ct), e -> drawSquare(row, col, visitedCode)));
+			t2.play();
+
 		}
 		return false;
 	}
-//    Commented below line nos.
-//    85
-//    120, 
-//    125-133
-//    248, 
-//    249
-//    257
-//    258-261
-//    178-181
-//    193-196
-//    Added below line nos.
-//    86
 }
